7427a415f0d0d6742a1034a428c2930db11dc796
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-03-05 10:29:08 -0500

    Update functions.py

--- momentum/functions.py
+++ momentum/functions.py
@@ -39,8 +39,8 @@ def kurtosis_update(m: dict, x: float) -> dict:
             m['count'] * m['count'] - 3 * m['count'] + 3) + 6 * delta_n2 * m['M2'] - 4 * delta_n * m['M3']
     m['M3'] = m['M3'] + term1 * delta_n * (m['count'] - 2) - 3 * delta_n * m['M2']
     m['M2'] = m['M2'] + term1
-    m['kurtosis'] = (m['count'] * m['M4']) / (m['M2'] * m['M2']) - 3
-    m['skewness'] = m['M3'] / m['M2']
+    m['kurtosis'] = ((m['count'] * m['M4']) / (m['M2'] * m['M2']) - 3 ) if m['count']>1 else 0
+    m['skewness'] = ( m['M3'] / m['M2'] ) if m['count']>1 else 0 
     m['var'] = m['M2'] / (m['count'] - 1) if m['count'] > 1 else 0
     m['pvar'] = m['M2'] / (m['count']) if m['count'] > 0 else 0
     m['std'] = math.sqrt(m['var']) if m['var'] > 0 else 0
